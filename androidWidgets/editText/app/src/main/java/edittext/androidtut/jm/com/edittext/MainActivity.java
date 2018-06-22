4713780d8ded26daeab31de5d19fb94831b9d0dd
mahajanjayesh <officialjayesh@gmail.com>
2018-06-22 17:33:29 +0530

    EditText

---
+++ androidWidgets/editText/app/src/main/java/edittext/androidtut/jm/com/edittext/MainActivity.java
@@ -0,0 +1,40 @@
+package edittext.androidtut.jm.com.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                String str = editText.getText().toString();
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+}
