3e958bc5af5d916abbc233582aae2671d5d311e6
mahajanjayesh <officialjayesh@gmail.com>
2018-06-22 19:12:36 +0530

    Seekbar

---
+++ androidWidgets/SeekBar/app/src/main/java/seekbar/androidtut/jm/com/seekbar/MainActivity.java
@@ -0,0 +1,32 @@
+package seekbar.androidtut.jm.com.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Toast.makeText(getApplicationContext(),"SeekBar Progress: "+i,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"SeekBar Started",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"SeekBar Stopped",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
